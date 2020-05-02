09a563321694e5953ae77075db90dc05ab0b5c95
Milos Pejanovic <pejan123@gmail.com>
2020-05-03 01:20:56 +0200

    Add button and keysym defs

---
+++ buttondef.go
@@ -0,0 +1,14 @@
+package i2vnc
+
+var Buttons map[string]uint8 = map[string]uint8{
+	"Button_None":   0,
+	"Button_Left":   1,
+	"Button_Middle": 2,
+	"Button_Right":  3,
+	"Button_Up":     4,
+	"Button_Down":   5,
+	"Button_6":      6,
+	"Button_7":      7,
+	"Button_8":      8,
+	"Button_9":      9,
+}
